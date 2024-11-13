72ceee5580de3e4d0b8cf0b1ddba2e6ad2f5a4bb
unknown <lyosprogramming@gmail.com>
2024-11-13 19:19:33 +0100

    Adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { Provider } from 'react-redux';
 
 import { store } from './redux/store';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import PhotoDetail from './pages/DetailsPage/DetailsPage';
 
 function App() {
   return (
     <>
     <Provider store={store}>
-      <div>Test</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/photo/:id" element={<PhotoDetail />} />
+      </Routes>
+    </Router>
+
     </Provider>
     </>
   );
