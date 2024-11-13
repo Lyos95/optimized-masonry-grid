19acfba1d99bc744bd979a290a857170b27ca453
unknown <lyosprogramming@gmail.com>
2024-11-13 19:09:23 +0100

    Adding project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths() // Add this plugin to help Vite resolve TypeScript paths
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 3,
+      },
+      mangle: true,
+    },
+    cssCodeSplit: true,
+  },
 })
